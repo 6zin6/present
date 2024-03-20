126cc75be29734e29876ce660f27e556f90fd4d6
zin <t.kawaguchi0907@gmail.com>
2024-03-21 00:57:51 +0900

    headerコンポーネントを追加しました

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <h1>present</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">デザイン</a>
+                    </li>
+                    <li>
+                        <a href="">プログラミング</a>
+                    </li>
+                    <li>
+                        <a href="">AI</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+};
+
+export default Header;
